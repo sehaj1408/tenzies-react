d82cca0d160405b6ae288a5ea1b8cc8ee92ceebb
Sehaj Naangal <snaangal@uwo.ca>
2023-01-17 13:07:27 -0500

    change color

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -5,7 +5,7 @@ export default function Die(props) {;
     return (
         <div 
         className="dice" 
-        style={{backgroundColor: props.isClicked ? "#a1bbff" : "white"}}
+        style={{backgroundColor: props.isClicked ? "#80d0ff" : "white"}}
         >
             <button 
             onClick={() => props.handleDiceClick(props.id)}
